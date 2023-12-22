4bbe5567c3bbc5e3eea37128917de89f3d2a9c51
Felix Omundi <omundifelix30@gmail.com>
2023-12-22 17:07:52 +0300

    first commit

--- database/models/user.js
+++ database/models/user.js
@@ -1,8 +1,29 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+import connection from '../connection';
+const {  Model} = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   User.init({
+//     firstName: DataTypes.STRING,
+//     lastName: DataTypes.STRING,
+//     email: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
+
+const initUser = (sequelize, DataTypes) => {
   class User extends Model {
     /**
      * Helper method for defining associations.
@@ -23,3 +44,5 @@ module.exports = (sequelize, DataTypes) => {
   });
   return User;
 };
+
+export default initUser(connection, DataTypes);
